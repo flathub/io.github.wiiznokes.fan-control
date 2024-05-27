6479c2b02f8a0e4084806b48025cb32bdcaf1853
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-05-27 22:55:22 +0200

    update deps (#3)
    * change res path
    
    * Update cargo-sources.json
    
    * update
    
    * update rev

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+rm -rf fan-control
+rm -rf flatpak-builder-tools
+
+
+git clone https://github.com/wiiznokes/fan-control.git
+git clone https://github.com/flatpak/flatpak-builder-tools
+
+pip install aiohttp toml
+python3 flatpak-builder-tools/cargo/flatpak-cargo-generator.py fan-control/Cargo.lock -o cargo-sources.json
+
+prev_rev=$(cat rev)
+new_rev=$(git -C fan-control rev-parse HEAD)
+echo $new_rev > rev
+
+patch=$(cat commit.patch)
+
+patch=$(echo "$patch" | sed "s/###1/$prev_rev/")
+patch=$(echo "$patch" | sed "s/###2/$new_rev/")
+
+
+echo "$patch" | git apply
