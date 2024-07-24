1a749d93883dc986615735dd3d1a33b05d2acb7f
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-07-24 22:35:20 +0200

    update

--- gen_manifest.sh
+++ gen_manifest.sh
@@ -6,7 +6,7 @@ set -x
 new_rev=$(git -C fan-control rev-parse HEAD)
 
 
-cat io.github.wiiznokes.fan-control.json.template | sed "s/###commit/$new_rev/" > io.github.wiiznokes.fan-control.json
+cat io.github.wiiznokes.fan-control.template.json | sed "s/###commit/$new_rev/" > io.github.wiiznokes.fan-control.json
 
 
 echo 'sha applied successfully'
